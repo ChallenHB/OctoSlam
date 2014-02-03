90c1a9287740468445a54fab920fefd8791bc535
crnhb38@gmail.com <crnhb38@gmail.com>
2014-02-03 15:28:14 -0500

    Playing with octomap functionality

--- src/read_map.cpp
+++ src/read_map.cpp
@@ -1,22 +1,27 @@
 #include <ros/ros.h>
 #include <octomap/octomap.h>
 #include <iostream>
+#include <fstream>
+
 int main() {
-    // How do I correctly open a octomap file
     octomap::OcTree* ot = new octomap::OcTree("data/map_sept30.bt");
-    std::cout << "Print something" << std::endl;
-    for(octomap::OcTree::leaf_iterator it = ot->begin_leafs(), end=ot->end_leafs(); it != end; ++it){
-        std::cout << "Node center: " << it.getCoordinate() << std::endl;
-        std::cout << "Node size: " << it.getSize() << std::endl;
-        std::cout << "Node value: " << it->getValue() << std::endl;
-    }
-   /* octomap::OcTreeNode *node = (octomap::OcTreeNode *)  ot->search(0, 0, 0);
+    octomap::OcTreeKey key = ot->coordToKey(.456, .5, .09 );
+    octomap::point3d point = ot->keyToCoord(key);
+    octomap::OcTreeNode* node = ot->search(point);
     if(node == NULL) {
         std::cout << "node is null" << std::endl;
     }
     else {
         std::cout << "node is not null" << std::endl;
+        std::cout << "node value: " << node->getValue() << std::endl;
     }
-    std::cout << "node value: " << node->getValue() << std::endl;*/
+    octomap::OcTreeNode* node1 = ot->search(0,1,0);
+    if(node1 == NULL) {
+        std::cout << "node1 is null" << std::endl;
+    }
+    else {
+        std::cout << "node1 is not null" << std::endl;
+        std::cout << "node1 value: " << node1->getValue() << std::endl;
+    }
     return 1;
 }
