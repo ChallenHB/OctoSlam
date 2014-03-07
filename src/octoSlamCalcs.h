ef79f929dfef358fcc9a07dbbbe12494f726e784
ChallenHB <crnhb38@gmail.com>
2014-03-07 14:07:04 -0500

    Unsuccessfully trying to chance the name of octoslamcalcs.h

--- src/octoSlamCalcs.h
+++ src/octoSlamCalcs.h
@@ -1,4 +1,4 @@
-#ifndef OCTOSLAM_CALCS
+fndef OCTOSLAM_CALCS
 #define OCTOSLAM_CALCS
 
 #include <Eigen/Dense>
@@ -10,16 +10,17 @@ namespace calculations {
     Eigen::Vector3f calc_det(float mv, float dx, float dy, float gammaP);
 
     Eigen::Matrix3f calc_hessian(float dx, float dy, float gammaP);
-    
+                
     float calc_gammaP(float gamma, float x, float y, float dx, float dy);
-   
+                    
     Vector3 calc_map_values(octomap::OcTree *map, octomath::Vector3 p,
                 std::vector<octomath::Vector3> vectors, float mres); 
-   
-    float calc_mres(octomap::OcTree *map, octomath::Vector3 endpoint);
+                              
+    float calc_mres(octomap::OcTree *map, octomath::Vector3
+                endpoint);
 
     std::vector<octomath::Vector3> round_voxels(octomath::Vector3 endpoint, float mres);
-    
+                                          
     std::vector<octomath::Vector3> transform_scan(const sensor_msgs::LaserScan::ConstPtr &scan, 
                 std::vector<double> current); 
 }
